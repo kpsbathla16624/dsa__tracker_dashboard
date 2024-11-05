daf1f0c77bdf419fc65c2331259adef40bc639c4
kamal <kamalpreet6198@gmail.com>
2024-11-05 18:30:26 +0530

    fix: add CORS middleware to allow requests from frontend URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/router';
+import cors from 'cors';
+
+
 
 
 
@@ -11,7 +14,11 @@ async function start() {
     try {
         const app = express();
         
-      
+        app.use(cors({
+            origin: 'https://dsa-tracker-dashboard-4ctx.vercel.app', // Allow requests only from your frontend's URL
+            methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods if necessary
+            credentials: true // Enable if your requests include credentials (cookies, etc.)
+          }));
         await mongoose.connect(process.env.MONGO_URL ?? '');
 
         app.use(express.json({ limit: '1000kb' }));
