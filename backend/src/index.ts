0fe669ecc03d0882398dbc302521fe98ede669fa
kamalpreet <kamalpreet6198@gmail.com>
2025-01-09 19:54:38 +0530

    gfg data route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,44 +1,33 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import router from './routes/router';
-import cors from 'cors';
-
-
-
-
-
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/router";
+import cors from "cors";
 dotenv.config();
-
 async function start() {
-    try {
-        const app = express();
-        // allow on all origins
-        app.use(cors(
-            {
-                origin: '*'
-            }
-        ));
-        await mongoose.connect(process.env.MONGO_URL ?? '');
+  try {
+    const app = express();
+    // allow on all origins
+    app.use(
+      cors({
+        origin: "*",
+      })
+    );
+    await mongoose.connect(process.env.MONGO_URL ?? "");
 
-        app.use(express.json({ limit: '1000kb' }));
+    app.use(express.json({ limit: "1000kb" }));
 
-        
-        app.use('/api', router); 
-        app.get('/', (req, res) => {
-            res.send('Hello World');
-        }
-        );
-        
+    app.use("/api", router);
+    app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
 
-        app.listen(3000, () => {
-            console.log('Server running on port 3000');
-        });
-       
-      
-    } catch (error) {
-        console.error('Database connection error:', error);
-    }
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
 }
 
 start();
--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -6,6 +6,7 @@ import leetcoderouter from './leetcodeRoutes';
 import CoderforcesRouter from './codeforcesRoutes';
 import codeChefRouter from './codechefRouter';
 import combinedDataRouter from './questionRoutes';
+import axios from 'axios';
 
 const router = Router();
 
@@ -16,4 +17,19 @@ router.use('/heatmap', combinedDataRouter );
 router.use('/leetcode',leetcoderouter);
 router.use('/codeforces',CoderforcesRouter);
 router.use('/codechef',codeChefRouter);
+router.get("/gfgdata", async (req, res) => {
+    try {
+      const url = "https://authapi.geeksforgeeks.org/api-get/user-profile-info/?handle=kamalprevsv1";
+      const response = await axios .get(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      res.json(response.data); // Forward the data to your frontend
+    } catch (error : any) {
+      console.error("Error fetching data:", error.message);
+      res.status(500).json({ error: "Failed to fetch data" });
+    }
+  });
+  
 export default router;
